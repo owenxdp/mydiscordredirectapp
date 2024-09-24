1b204ba64f9959ea9c7c12f60b8035f1ea1106ed
owenxdp <phoominphosuwan2692000@gmail.com>
2024-09-24 16:28:20 +0700

    edit /login

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
 const express = require("express");
 const app = express();
+const axios = require("axios");
+const url = require("url");
 let port = process.env.PORT || 3000;
 
-app.get("/api/auth/discord/redirect", (req, res) => {
-    res.send(req.query);
+app.get("/login", (req, res) => {
+    const {code} = req.query;
+    if (code){
+        res.send(code)
+    }else{
+        res.redirect("https://discord.com/oauth2/authorize?client_id=1287818726942703777&response_type=code&redirect_uri=http%3A%2F%2Flocalhost%3A3000%2Flogin&scope=identify");
+    }
 })
 
 app.listen(port, () => {
-    console.log(`Example app listenung on port ${3000}`);
+    console.log(`App listenung on port ${3000}`);
 })
